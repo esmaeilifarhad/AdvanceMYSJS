72bf24819c60e21c1ec320637e8d533e0b9dbd64
farhad <esmaili.farhad67@gmail.com>
2020-09-26 13:44:58 +0330

    Before Add JS Files

---
+++ AdvanceMYS/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceMYS.Controllers
+{
+    public class TaskController : Controller
+    {
+        Models.Domain._5069_ManageYourSelfContext db = new Models.Domain._5069_ManageYourSelfContext();
+       
+        public IActionResult Index()
+        {
+           var res= db.Tasks.ToList();
+            return Json(res);
+        }
+        
+    }
+}
